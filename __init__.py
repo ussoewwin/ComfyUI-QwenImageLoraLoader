1b777450bd049bdf22141f1faaf69c7ffe8bf53b
ussoewwin <136552381+ussoewwin@users.noreply.github.com>
2026-02-07 23:18:12 +0900

    feat: Z-Image-Turbo LoRA Stack V1 (rgthree-style), Issue #12 link in changelog, v2.4.0 release notes

--- __init__.py
+++ __init__.py
@@ -20,6 +20,8 @@ QWEN_V2_NODES = {}
 QWEN_V2_NAMES = {}
 QWEN_V3_NODES = {}
 QWEN_V3_NAMES = {}
+ZIMAGETURBO_V1_NODES = {}
+ZIMAGETURBO_V1_NAMES = {}
 ZIMAGETURBO_V2_NODES = {}
 ZIMAGETURBO_V2_NAMES = {}
 ZIMAGETURBO_V4_NODES = {}
@@ -41,6 +43,7 @@ try:
     from .nodes.lora.qwenimage_v2 import GENERATED_NODES as QWEN_V2_NODES, GENERATED_DISPLAY_NAMES as QWEN_V2_NAMES
     from .nodes.lora.qwenimage_v3 import GENERATED_NODES as QWEN_V3_NODES, GENERATED_DISPLAY_NAMES as QWEN_V3_NAMES
     from .nodes.lora.qwenimage_v1 import GENERATED_NODES as QWEN_V1_NODES, GENERATED_DISPLAY_NAMES as QWEN_V1_NAMES
+    from .nodes.lora.zimageturbo_v1 import GENERATED_NODES as ZIMAGETURBO_V1_NODES, GENERATED_DISPLAY_NAMES as ZIMAGETURBO_V1_NAMES
     from .nodes.lora.zimageturbo_v2 import GENERATED_NODES as ZIMAGETURBO_V2_NODES, GENERATED_DISPLAY_NAMES as ZIMAGETURBO_V2_NAMES
     # Z-Image-Turbo V3 is deprecated - removed from registration
     # from .nodes.lora.zimageturbo_v3 import GENERATED_NODES as ZIMAGETURBO_V3_NODES, GENERATED_DISPLAY_NAMES as ZIMAGETURBO_V3_NAMES
@@ -55,6 +58,8 @@ try:
         node_class.__version__ = __version__
     for node_class in QWEN_V1_NODES.values():
         node_class.__version__ = __version__
+    for node_class in ZIMAGETURBO_V1_NODES.values():
+        node_class.__version__ = __version__
     for node_class in ZIMAGETURBO_V2_NODES.values():
         node_class.__version__ = __version__
     # Z-Image-Turbo V3 registration removed
@@ -68,6 +73,7 @@ try:
     NODE_CLASS_MAPPINGS.update(QWEN_V2_NODES)
     NODE_CLASS_MAPPINGS.update(QWEN_V3_NODES)
     NODE_CLASS_MAPPINGS.update(QWEN_V1_NODES)
+    NODE_CLASS_MAPPINGS.update(ZIMAGETURBO_V1_NODES)
     NODE_CLASS_MAPPINGS.update(ZIMAGETURBO_V2_NODES)
     # Z-Image-Turbo V3 registration removed
     # NODE_CLASS_MAPPINGS.update(ZIMAGETURBO_V3_NODES)
@@ -81,6 +87,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     **QWEN_V2_NAMES,
     **QWEN_V3_NAMES,
     **QWEN_V1_NAMES,
+    **ZIMAGETURBO_V1_NAMES,
     **ZIMAGETURBO_V2_NAMES,
     # Z-Image-Turbo V3 registration removed
     # **ZIMAGETURBO_V3_NAMES,
